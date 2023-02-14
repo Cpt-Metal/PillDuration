ce18a5f394473e9fe63f2a8952266e2c6eea61c3
MetalTrump <46907438+MetalTrump@users.noreply.github.com>
2023-02-14 14:10:25 +0100

    fixed ISTakePillAction override
    should no longer break other mods

--- media/lua/client/TimedActions/pillActionEdit.lua
+++ media/lua/client/TimedActions/pillActionEdit.lua
@@ -6,10 +6,12 @@ require "TimedActions/ISBaseTimedAction"
 require "TimedActions/ISTakePillAction"
 require "floatingInfoWindow"
 
+local oldISTakePillAction_perform = ISTakePillAction.perform
 function ISTakePillAction:perform()
-    self.item:getContainer():setDrawDirty(true);
-    self.item:setJobDelta(0.0);
-    self.character:getBodyDamage():JustTookPill(self.item);
-	ISBaseTimedAction.perform(self);
+    --self.item:getContainer():setDrawDirty(true);
+    --self.item:setJobDelta(0.0);
+    --self.character:getBodyDamage():JustTookPill(self.item);
+	--ISBaseTimedAction.perform(self);
+	oldISTakePillAction_perform(self)
 	updatePillInfoWindow();
 end
